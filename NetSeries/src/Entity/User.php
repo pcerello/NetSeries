a54b7adef0fd234da13631b64108d64866466a99
Benjamin Gandelin <benjamin.gandelin@etu.u-bordeaux.fr>
2023-01-05 17:45:03 +0100

    add country infos

--- NetSeries/src/Entity/User.php
+++ NetSeries/src/Entity/User.php
@@ -2,9 +2,7 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -71,7 +69,7 @@ class User implements PasswordAuthenticatedUserInterface, UserInterface
     private $userId;
 
     /**
-     * @var \Country
+     * @var \Country|null
      *
      * @ORM\ManyToOne(targetEntity="Country")
      * @ORM\JoinColumns({
--- NetSeries/src/Form/RegistrationFormType.php
+++ NetSeries/src/Form/RegistrationFormType.php
@@ -19,6 +19,7 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('email')
             ->add('name')
+            ->add('country')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
