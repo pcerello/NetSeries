5d3cc2b17c841399ce50232993aaba4906d94d0a
Raphael Alarcon <raphael.alarcon@MacBook-Pro-de-Raphael.local>
2023-01-09 17:56:50 +0100

    Modification page d'accueil

--- NetSeries/src/Controller/SeriesController.php
+++ NetSeries/src/Controller/SeriesController.php
@@ -6,13 +6,11 @@ use App\Entity\Episode;
 use App\Entity\Series;
 use App\Form\SeriesType;
 use App\Repository\EpisodeRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Persistence\ManagerRegistry;
 
 use Knp\Component\Pager\PaginatorInterface;
 
