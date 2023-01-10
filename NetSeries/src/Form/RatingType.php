9178c4e1a1712db055d46cb7111cbe0018a57484
ykadri <yanis.kadri@etu.u-bordeaux.fr>
2023-01-10 21:00:37 +0100

    Menu déroulant pour choix de la note qui fonctionne

--- NetSeries/src/Form/RatingType.php
+++ NetSeries/src/Form/RatingType.php
@@ -6,16 +6,32 @@ use App\Entity\Rating;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints\Choice;
 
 class RatingType extends AbstractType
 {
+    
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('value')
-            ->add('comment')
-         
-        ;
+            ->add('value', ChoiceType::class, [ 'choices' => [
+                '' => '',
+                '0' => 0,
+                '1' => 1,
+                '2' => 2,
+                '3' => 3,
+                '4' => 4,
+                '5' => 5,
+                '6' => 6,
+                '7' => 7,
+                '8' => 8,
+                '9' => 9,
+                '10' => 10,
+            ],
+            'constraints' => [new Choice(['choices' => [1, 2, 3, 4, 5]])]
+        ])
+            ->add('comment');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
