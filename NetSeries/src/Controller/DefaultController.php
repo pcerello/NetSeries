82b46b00919d54e549e8df419e45c28f3bb8db49
Raphael Alarcon <raphael.alarcon@MacBook-Pro-de-Raphael.local>
2023-01-13 16:55:56 +0100

    Correction de la requête de la page d'accueil

--- NetSeries/src/Controller/DefaultController.php
+++ NetSeries/src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Series;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,14 +17,42 @@ class DefaultController extends AbstractController
     }
 
     #[Route('/', name: 'app_home')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(EntityManagerInterface $em): Response
     {
 
-        // Select 4 random series out of all series (optimized)
-        $randomSeries = $this->doctrine->getRepository(Series::class)->findBy([], [], 4);
+        // Select 4 random series using native sql
+        $sql = "SELECT * FROM series ORDER BY RAND() LIMIT 4";
+
+        // Create the ResultSetMapping object
+        $rsm = new \Doctrine\ORM\Query\ResultSetMapping;
+
+        // Add entity results
+        $rsm->addEntityResult(Series::class, 's');
+
+        // Map data columns to entity properties
+        $rsm->addFieldResult('s', 'id', 'id');
+        $rsm->addFieldResult('s', 'title', 'title');
+        $rsm->addFieldResult('s', 'description', 'description');
+        $rsm->addFieldResult('s', 'image', 'image');
+
+        // Create the query
+        $query = $em->createNativeQuery($sql, $rsm);
+
+        $search = $query->getResult();
+
+        // Convert the result to an array
+        $search = array_map(function ($series) {
+            return [
+                'id' => $series->getId(),
+                'title' => $series->getTitle(),
+                'description' => $series->getPlot(),
+                'image' => $series->getPoster(),
+                'externalRating' => $series->getExternalRating(),
+            ];
+        }, $search);
 
         return $this->render('home/index.html.twig', [
-            'random_series' => $randomSeries,
+            'random_series' => $search,
         ]);
     }
 }
