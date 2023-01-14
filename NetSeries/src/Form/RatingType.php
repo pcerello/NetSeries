fbc4bbd4752e3e4b07991b532eeb0cdd7013c535
ykadri <yanis.kadri@etu.u-bordeaux.fr>
2023-01-14 17:59:05 +0100

    problème de choix de note qui ne marcher pas qui fonctionne

--- NetSeries/src/Form/RatingType.php
+++ NetSeries/src/Form/RatingType.php
@@ -29,7 +29,7 @@ class RatingType extends AbstractType
                 '5 ⭐' => 10,
             ],
             'label' => 'Rating of series',
-            'constraints' => [new Choice(['choices' => [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]])]
+            'constraints' => [new Choice(['choices' => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])]
         ])
             ->add('comment');
     }
