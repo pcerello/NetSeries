b0da672d015b9b149b5f5502ddd7829af62c88a1
Raphael Alarcon <raphael.alarcon@MacBook-Pro-de-Raphael.local>
2023-01-16 11:37:38 +0100

    Responsive page d'accueil menu login register

--- NetSeries/public/js/app.js
+++ NetSeries/public/js/app.js
@@ -1,16 +1,21 @@
-var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
+window.onload = function(){
+  var sidenav = document.getElementById("buttons");
+  var openBtn= document.getElementById("burger");
 
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
+  openBtn.onclick = openNav;
 
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  sidenav.classList.add("active");
-}
+  /* Set the width of the side navigation to 250px */
+  function openNav() {
+    sidenav.classList.add("active");
+    openBtn.classList.add("close");
+    openBtn.onclick = closeNav;
+  }
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  sidenav.classList.remove("active");
-}
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    sidenav.classList.remove("active");
+    console.log("test");
+    openBtn.classList.remove("close");
+    openBtn.onclick = openNav;
+  }
+};
